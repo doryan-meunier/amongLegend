ebea23e9880383c6ef5788bed9d1f580e166eca9
Eldunari17 <axel.fargue@gmail.com>
2024-01-17 17:12:49 +0100

    boostrap + index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+</form>
+
+</body>
+</html>
