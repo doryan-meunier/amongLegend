3734026be3627d8570ccebd4d22efcc6ff38bde8
Eldunari17 <axel.fargue@gmail.com>
2024-01-18 20:56:50 +0100

    sélection cartes rôles créerPartie

--- js/creerPartie.js
+++ js/creerPartie.js
@@ -1,9 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
+function carte(idcarte) {
+    var carte = document.getElementById(idcarte);
+    console.log(carte.style.backgroundColor);
+    if (carte.style.backgroundColor == 'rgba(0, 255, 51, 0.46)')
+        deselectionCarte(carte);
+    else
+        selectionCarte(carte);
+}
 
-    function expand(elementId) {
-        document.getElementById(elementId).value = "";;
-    }
-    
-    
-    
-    });
+function selectionCarte(carte) {
+    carte.style.backgroundColor = 'rgba(0, 255, 51, 0.46)';
+}
+
+function deselectionCarte(carte) {
+    carte.style.backgroundColor = 'white';
+}
