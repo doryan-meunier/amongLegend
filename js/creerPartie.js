5dfada3f903e8fcf729eb24e8d89ae08f655871a
Eldunari17 <axel.fargue@gmail.com>
2024-01-18 21:11:24 +0100

    Revert "sélection cartes rôles créerPartie"
    This reverts commit 3734026be3627d8570ccebd4d22efcc6ff38bde8.

--- js/creerPartie.js
+++ js/creerPartie.js
@@ -1,16 +1,9 @@
-function carte(idcarte) {
-    var carte = document.getElementById(idcarte);
-    console.log(carte.style.backgroundColor);
-    if (carte.style.backgroundColor == 'rgba(0, 255, 51, 0.46)')
-        deselectionCarte(carte);
-    else
-        selectionCarte(carte);
-}
+document.addEventListener("DOMContentLoaded", function () {
 
-function selectionCarte(carte) {
-    carte.style.backgroundColor = 'rgba(0, 255, 51, 0.46)';
-}
-
-function deselectionCarte(carte) {
-    carte.style.backgroundColor = 'white';
-}
+    function expand(elementId) {
+        document.getElementById(elementId).value = "";;
+    }
+    
+    
+    
+    });
