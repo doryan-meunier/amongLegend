70e41ad903cefeec76958ac400ebcb36604988e7
Eldunari17 <axel.fargue@gmail.com>
2024-01-19 13:59:16 +0100

    changement de fond créerPartie

--- js/creerPartie.js
+++ js/creerPartie.js
@@ -1,16 +1,24 @@
 function carte(idcarte) {
     var carte = document.getElementById(idcarte);
-    console.log(carte.style.backgroundColor);
-    if (carte.style.backgroundColor == 'rgba(0, 255, 51, 0.46)')
+    if (carte.style.backgroundColor == 'rgb(138, 255, 161)')
         deselectionCarte(carte);
     else
         selectionCarte(carte);
 }
 
 function selectionCarte(carte) {
-    carte.style.backgroundColor = 'rgba(0, 255, 51, 0.46)';
+    carte.style.backgroundColor = 'rgb(138, 255, 161)';
 }
 
 function deselectionCarte(carte) {
     carte.style.backgroundColor = 'white';
+}
+
+function toggleFond() {
+    var body = document.getElementById('body');
+    bodyBackground = body.style.backgroundImage;
+    if (bodyBackground = 'url("https://raw.githubusercontent.com/doryan-meunier/amongLegend/main/images/creerPartie/howlingAbyss.jpeg")')
+        bodyBackground = 'url("https://raw.githubusercontent.com/doryan-meunier/amongLegend/main/images/creerPartie/SummonersRift.jpeg")';
+    else
+        bodyBackground = 'url("https://raw.githubusercontent.com/doryan-meunier/amongLegend/main/images/creerPartie/howlingAbyss.jpeg")';
 }
